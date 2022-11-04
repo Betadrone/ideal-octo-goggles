3c888bd2c5adc5fa5dda92ef1be80fc51cad1ba6
Muhammad Ashhad <113926178+Betadrone@users.noreply.github.com>
2022-11-04 16:54:40 -0400

    Update CombinedCode.cpp

--- CombinedCode.cpp
+++ CombinedCode.cpp
@@ -3,8 +3,6 @@
 // ProblemSolving5b
 
 #include <iostream>
-using namespace std;
-
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
@@ -12,25 +10,35 @@ using namespace std;
 #include <cctype>
 #include <windows.h>
 
+using namespace std;
+
+// Global variables
 int playerChoice = 0;
 int userInput = 0;
 int bossAtk = 2; // Boss stats
 int heroMp = 10;
 unsigned int mp = 10; //Initial magic points
-float bossHpf = 50;
-float heroHpf = 20;
+float bossHpf = 50.0f;
+float heroHpf = 20.0f;
+float chargeAtk = 4.5f;
 bool bossEnrage = false;
 bool repeat = false;
 bool pendant = false;
+bool shield = false;
+bool charge = false;
+bool skipBoss = false;
+bool run = false;
 
+//
 void Intro();
-void bossMoves();
+void gameplay();
 void GiveOptions();
 void ManaOptions();
 void lowMana();
-float stats(float heroH, float bossH, int heroM);
+void Shieldtxt();
+float stats(float heroH, float bossH, int heroM, bool rage);
 
-
+// Main Function Crackhead
 int main()
 {
 	Sleep(1500);
@@ -52,7 +60,7 @@ int main()
 	{
 		system("cls");
 		Intro();
-		bossMoves();
+		gameplay();
 		if (heroHpf <= 0 && pendant == false)
 		{
 			cout << "You have died..." << endl;
@@ -79,10 +87,10 @@ int main()
 			system("cls");
 
 			pendant = false;
-			heroHpf = 10;
-			heroMp = 5;
+			heroHpf += 10;
+			mp = 10;
 
-			bossMoves();
+			gameplay();
 
 			if (heroHpf <= 0 && pendant == false)
 			{
@@ -143,32 +151,30 @@ void Intro()
 		cout << "You decide to move on towards the boss gate..." << endl;
 
 	}
+	system("pause");
+	system("cls");
+	cout << "As soon as you get close to the gate, it glows brightly and you find yourself teleported in front of an Orc like being" << endl;
+	system("pause");
+	system("cls");
+	cout << "You realize it's the boss..." << endl;
 }
 
-float stats(float heroH, float bossH, int heroM)
+float stats(float heroH, float bossH, int heroM, bool rage)
 {
 	// Got the color codes from: https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
 	cout << "Stat Overview" << endl;
 	cout << "---------------------" << endl;
-	if (bossH >= 31) // Green color next to hp
+	if (bossH <= 0)
 	{
-		cout << "\u001b[42m" << "  " << "\u001b[40m" << " Hp: " << bossH << "/50 " << "\u001b[42m" << "  " << "\u001b[40m" << endl;
+		cout << "\u001b[47m" << "  " << "\u001b[40m" << " Boss Hp: " << bossH << "/50 " << "\u001b[47m" << "  " << "\u001b[40m" << endl;
 	}
-	else if (bossH >= 30) // This is a repeat of the next else if statement, but does not include an extra space after the /20
+	else if (rage == true)
 	{
-		cout << "\u001b[43m" << "  " << "\u001b[40m" << " Hp: " << bossH << "/50 " << "\u001b[43m" << "  " << "\u001b[40m" << endl;
+		cout << "\u001b[46m" << "  " << "\u001b[40m" << " Boss Hp: " << bossH << "/50 " << "\u001b[46m" << "  " << "\u001b[40m" << endl;
 	}
-	else if (bossH >= 15) // Yellow colour next to hp
+	else // Magenta color next to hp
 	{
-		cout << "\u001b[43m" << "  " << "\u001b[40m" << " Hp: " << bossH << "/50  " << "\u001b[43m" << "  " << "\u001b[40m" << endl;
-	}
-	else if (bossH > 0) // Red colour next to hp
-	{
-		cout << "\u001b[41m" << "  " << "\u001b[40m" << " Hp: " << bossH << "/50  " << "\u001b[41m" << "  " << "\u001b[40m" << endl;
-	}
-	else // White color next to hp
-	{
-		cout << "\u001b[47m" << "  " << "\u001b[40m" << " Hp: " << bossH << "/50  " << "\u001b[47m" << "  " << "\u001b[40m" << endl;
+		cout << "\u001b[45m" << "  " << "\u001b[40m" << " Boss Hp: " << bossH << "/50 " << "\u001b[45m" << "  " << "\u001b[40m" << endl;
 	}
 
 	cout << endl;
@@ -211,7 +217,7 @@ float stats(float heroH, float bossH, int heroM)
 }
 
 
-void bossMoves()
+void gameplay()
 {
 
 	enum MOVES //This is for the options first presented to the pl
@@ -219,14 +225,18 @@ void bossMoves()
 		ATTACK = 1,
 		DEFEND,
 		MAGIC,
-		RUN
+		RUN,   // This will act as a Setinel Value
+
+		NUM_MOVES,
 	};
 
 	enum CHOICE
 	{
 		ENCHANT = 1,
 		DISABLE,
-		HEAL
+		HEAL,
+
+		NUM_MAGIC,
 	};
 
 	enum BOSS_ACTIONS
@@ -241,15 +251,17 @@ void bossMoves()
 		NUM_BOSS_ACTIONS,
 	};
 
-	stats(heroHpf, bossHpf, mp);
 
-	while (bossHpf > 0.0f && heroHpf > 0.0f)
+	for (int i = 0; run == false && heroHpf > 0.0f; i++) // i is the temporary integer we will be using as a turn counter 
 	{
-
 		//Hero attacks
 		do
 		{
+			cout << "Turn: " << i << endl;
+			stats(heroHpf, bossHpf, mp, bossEnrage);
+
 			repeat = false;
+			shield = false;
 
 			GiveOptions();
 			cin >> playerChoice;
@@ -259,13 +271,15 @@ void bossMoves()
 
 			case ATTACK:
 			{
-				cout << "Player Attacks!" << endl;
+				cout << "Player Attacks! +2 Atk" << endl;
+				bossHpf -= 2;
 				break;
 			}
 
 			case DEFEND:
 			{
-				cout << "Player Defends!" << endl;
+				cout << "You raise your shield!" << endl;
+				shield = true;
 				if (mp >= 10)
 				{
 					cout << "Your Mp is full!" << endl;
@@ -288,10 +302,11 @@ void bossMoves()
 
 				case ENCHANT:
 				{
-					if (mp >= 3)
+					if (mp >= 2)
 					{
-						cout << "Player enchants his sword and Attacks!" << endl;
-						mp -= 3;
+						cout << "Player enchants his sword and Attacks! +5 Atk" << endl;
+						mp -= 2;
+						bossHpf -= 5;
 					}
 					else
 					{
@@ -303,10 +318,12 @@ void bossMoves()
 
 				case DISABLE:
 				{
-					if (mp >= 6)
+					if (mp >= 4)
 					{
 						cout << "Player restrains the monster for one turn!" << endl;
-						mp -= 6;
+						mp -= 4;
+						skipBoss = true;
+
 					}
 					else
 					{
@@ -318,10 +335,10 @@ void bossMoves()
 
 				case HEAL:
 				{
-					if (mp >= 8 && heroHpf <= 19)
+					if (mp >= 4 && heroHpf <= 19)
 					{
 						cout << "Player recovers +10 hp!" << endl;
-						mp -= 8;
+						mp -= 4;
 						heroHpf += 10;
 						if (heroHpf >= 20)
 						{
@@ -357,6 +374,7 @@ void bossMoves()
 			case RUN:
 			{
 				cout << "Player runs away!" << endl;
+				run = true;
 				break;
 			}
 
@@ -368,83 +386,135 @@ void bossMoves()
 			}
 		} while (repeat == true);
 
-		//Boss attacks
-		srand(time(0)); // creates seed
-		int bossAct = (rand() % 100) + 1;
-
-		if (bossAct >= 1 && bossAct <= 66)
+		if (run == true)
 		{
-			bossAct = 1;
+			cout << "You got away Safely" << endl;
 		}
-		else if (bossAct >= 67 && bossAct <= 89)
+		else if (bossHpf > 0.0f)
 		{
-			bossAct = 2;
 
 		}
-		else if (bossAct >= 90 && bossAct <= 98)
+		else
 		{
-			bossAct = 3;
+			//Boss attacks
+			srand(time(0)); // creates seed
+			int bossAct = (rand() % 100) + 1;
 
-		}
-		else if (bossAct >= 99 && bossAct <= 100)
-		{
-			bossAct = 4;
-		}
-
-		switch (bossAct)
-		{
-		case BOSS_ATTACK:
-		{
-			cout << "[The Boss attacked the player normally...]" << endl;
-			heroHpf -= bossAtk;
-			break;
-		}
-		case CHARGED_ATTACK:
-		{
-			cout << "[The Boss attacked the player fiercly...]" << endl;
-			heroHpf -= (bossAtk + 1);
-			break;
-		}
-		case BOSS_HEAL:
-		{
-			cout << "[The boss recovered some of its HP]" << endl;
-			if (bossHpf < 50)
+			if (bossAct >= 1 && bossAct <= 66)
 			{
-				bossHpf += 5;
+				bossAct = 1;
 			}
-			else if (bossHpf >= 50)
+			else if (bossAct >= 67 && bossAct <= 89)
 			{
-				bossHpf = 50;
+				bossAct = 2;
+
 			}
-			break;
-		}
-		case FLINCH:
-		{
-			cout << "[The Boss flinched and couldn't do anything]" << endl;
-			break;
-		}
-		}
+			else if (bossAct >= 90 && bossAct <= 91)
+			{
+				bossAct = 3;
 
-		stats(heroHpf, bossHpf, mp);
+			}
+			else if (bossAct >= 92 && bossAct <= 100)
+			{
+				bossAct = 4;
+			}
 
+			if (skipBoss == true)
+			{
+				charge = false;
+				skipBoss = false;
+			}
+			else if (charge == true)    // This is for the boss' charge Attack. It's normal attacks will not be available. 
+			{
+				if (shield == true)
+				{
+					heroHpf -= chargeAtk / 2;
+					Shieldtxt();
+					cout << "-" << chargeAtk / 2 << " Hp" << endl;
+				}
+				else
+				{
+					heroHpf -= chargeAtk;
+				}
+				charge = false;
+			}
+			else
+			{
+				switch (bossAct)
+				{
+				case BOSS_ATTACK:
+				{
+					cout << "[The Boss attacked the player normally...]" << endl;
+					if (shield == true)
+					{
+						heroHpf -= (bossAtk / 2);
+						Shieldtxt();
+						cout << "-" << bossAtk / 2 << " Hp" << endl;
+					}
+					else
+					{
+						heroHpf -= bossAtk;
+					}
+					break;
+				}
+				case CHARGED_ATTACK:
+				{
+					cout << "[The Boss attacked the player fiercly...]" << endl;
+					charge = true;
+					break;
+				}
+				case BOSS_HEAL:
+				{
+					cout << "[The boss recovered some of its HP]" << endl;
+					if (bossHpf < 50)
+					{
+						bossHpf += 5;
+					}
+					else if (bossHpf >= 50)
+					{
+						bossHpf = 50;
+					}
+					break;
+				}
+				case FLINCH:
+				{
+					cout << "[The Boss flinched and couldn't do anything]" << endl;
+					break;
+				}
+				}
+			}
 
+			if (bossHpf <= 20 && bossEnrage == false)
+			{
+				system("cls");
+				bossEnrage = true;
+				cout << "Boss has become enraged" << endl;
+				chargeAtk *= 2;
+				bossAtk *= 2;
+
+				system("pause");
+			}
+		}
 	}
-
-	
 }
+
 void GiveOptions()
 {
 	cout << "Please choose a move:" << endl;
-	cout << "[1] Attack \t [3] Magic" << endl;
-	cout << "[2] Defend \t [4] Run" << endl;
+	cout << "[1] Attack         \t [3] Magic" << endl;
+	cout << "[2] Defend [+1 Mp] \t [4] Run" << endl;
 }
 void ManaOptions()
 {
 	cout << "What will you do?" << endl;
-	cout << "[1] Enchant \t [3] Heal" << endl;
-	cout << "[2] Disable \t [4] Back" << endl;
+	cout << "[1] Enchant [3 Mp] \t [3] Heal [4 Mp]" << endl;
+	cout << "[2] Disable [4 Mp] \t [4] Back" << endl;
 }
 void lowMana()
 {
 	cout << "You do not have enough Mp!" << endl;
+}
+void Shieldtxt()
+{
+	cout << "Your shield was up!";
 }
